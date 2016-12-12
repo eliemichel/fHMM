20ce5fcdbe82d03e3c13b138676d7e52e11cebdb
Élie Michel <elie.michel@ens.fr>
2016-12-12 16:13:43 +0100

    Small syntactic changes

--- src/generative/generative_fun.py
+++ src/generative/generative_fun.py
@@ -15,14 +15,14 @@ def generative_fun(pi_l,A_l,C_m,W_l,T):
     X_m=np.zeros((M,T+1))
     Y_m=np.zeros((D,T+1))
     
-    for m in np.arange(M):
+    for m in range(M):
         r=random.random()
         cumpim_v=np.cumsum(pi_l[m])
         ind_v=(r<cumpim_v)*1
         indm=np.argmax(ind_v)
         X_m[m,0]=indm
     
-    for t in np.arange(T)+1:
+    for t in range(1, T+1):
         Xoldt_v=X_m[:,t-1]
         for m in np.arange(M):
             Am_m=A_l[m]
@@ -35,7 +35,7 @@ def generative_fun(pi_l,A_l,C_m,W_l,T):
             indtm=np.argmax(ind_v)
             X_m[m,t]=indtm
             
-    for t in np.arange(T+1):
+    for t in range(T+1):
         mut_v=0
         for m in np.arange(M):
             mut_v+=W_l[m][:,X_m[m,t]]
